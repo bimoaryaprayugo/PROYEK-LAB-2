889a316fae24da27d9683d3113282bc79623e1b5
Fandy <fandymsimanjorang@gmail.com>
2025-12-18 16:54:07 +0700

    Add files via upload

--- uas.pas
+++ uas.pas
@@ -3,7 +3,7 @@ program NilaiMahasiswa;
 uses crt;
 
 type
-TMahasiswa = record
+  TMahasiswa = record
     nim   : string[10];
     nama  : string[30];
     tugas : integer;
@@ -11,11 +11,11 @@ TMahasiswa = record
     uas   : integer;
     na    : real;
     grade : char;
-end;
+  end;
 
 var
-    mhs  : array[1..100] of TMahasiswa;
-    n, i, pilih : integer;
+  mhs  : array[1..100] of TMahasiswa;
+  n, i, pilih : integer;
 
 { Fungsi untuk hitung Nilai Akhir }
 function HitungNA(tugas, uts, uas: integer): real;
@@ -26,27 +26,27 @@ end;
 { Fungsi untuk menentukan Grade }
 function TentukanGrade(na: real): char;
 begin
-if na >= 80 then
+  if na >= 80 then
     TentukanGrade := 'A'
-else if na >= 70 then
+  else if na >= 70 then
     TentukanGrade := 'B'
-else if na >= 60 then
+  else if na >= 60 then
     TentukanGrade := 'C'
-else if na >= 50 then
+  else if na >= 50 then
     TentukanGrade := 'D'
-else
+  else
     TentukanGrade := 'E';
 end;
 
 { Prosedur Input Data Mahasiswa }
 procedure InputData;
 begin
-clrscr;
-write('Masukkan jumlah mahasiswa: ');
-readln(n);
+  clrscr;
+  write('Masukkan jumlah mahasiswa: ');
+  readln(n);
 
-for i := 1 to n do
-begin
+  for i := 1 to n do
+  begin
     writeln('--- Data Mahasiswa ke-', i, ' ---');
 
     write('Nama   : ');
@@ -66,42 +66,42 @@ begin
 
     mhs[i].na := HitungNA(mhs[i].tugas, mhs[i].uts, mhs[i].uas);
     mhs[i].grade := TentukanGrade(mhs[i].na);
-    end;
+  end;
 
-    writeln('Data berhasil diinput...');
-    readln;
+  writeln('Data berhasil diinput...');
+  readln;
 end;
 
 { Prosedur Tampil Data Mahasiswa (Output Rapi) }
 procedure TampilData;
 begin
-    clrscr;
-    writeln('====================================================================');
-    writeln('NIM         Nama           Tugas   UTS   UAS    NA    Grade');
-    writeln('====================================================================');
+  clrscr;
+  writeln('====================================================================');
+  writeln('NIM         Nama           Tugas   UTS   UAS    NA    Grade');
+  writeln('====================================================================');
 
     for i := 1 to n do
     begin
     writeln(mhs[i].nim, ' ',
-            mhs[i].nama:18, ' ',
-            mhs[i].tugas:6, ' ',
-            mhs[i].uts:5, ' ',
-            mhs[i].uas:5, ' ',
-            mhs[i].na:6:1, ' ',
-            mhs[i].grade);
-        end;
+          mhs[i].nama:18, ' ',
+          mhs[i].tugas:6, ' ',
+          mhs[i].uts:5, ' ',
+          mhs[i].uas:5, ' ',
+          mhs[i].na:6:1, ' ',
+          mhs[i].grade);
+    end;
 
-    readln;
+  readln;
 end;
 
 {Prosedur Cari Mahasiswa}
 procedure CariMahasiswa;
 var
-    cariNIM : string;
-    ketemu  : boolean;
-    pilihan : char;
+  cariNIM : string;
+  ketemu  : boolean;
+  pilihan : char;
 begin
-    repeat
+  repeat
     clrscr;
     write('Masukkan NIM yang dicari: ');
     readln(cariNIM);
@@ -110,42 +110,42 @@ begin
 
     for i := 1 to n do
     begin
-        if mhs[i].nim = cariNIM then
-        begin
+      if mhs[i].nim = cariNIM then
+      begin
         writeln('Data Ditemukan!');
         writeln('Nama        : ', mhs[i].nama);
-        writeln('Nilai Akhir : ', mhs[i].na:6:1);
+        writeln('Nilai Akhir : ', mhs[i].na:3:1);
         writeln('Grade       : ', mhs[i].grade);
         ketemu := true;
-        end;
+      end;
     end;
 
     if ketemu = false then
     begin
-        writeln;
-        writeln('Data tidak ditemukan.');
-        writeln('1. Cari lagi');
-        writeln('2. Kembali ke menu utama');
-        write('Pilih: ');
-        readln(pilihan);
+      writeln;
+      writeln('Data tidak ditemukan.');
+      writeln('1. Cari lagi');
+      writeln('2. Kembali ke menu utama');
+      write('Pilih: ');
+      readln(pilihan);
 
-        if pilihan = '2' then
+      if pilihan = '2' then
         exit;
     end
     else
     begin
-        writeln;
-        writeln('Tekan ENTER untuk kembali ke menu utama...');
-        readln;
-        exit;
+      writeln;
+      writeln('Tekan ENTER untuk kembali ke menu utama...');
+      readln;
+      exit;
     end;
 
-    until false;
+  until false;
 end;
 
 { Menu Utama Program }
 begin
-    repeat
+  repeat
     clrscr;
     writeln('=== PROGRAM NILAI MAHASISWA ===');
     writeln('1. Input Data');
@@ -156,10 +156,10 @@ begin
     readln(pilih);
 
     case pilih of
-    1: InputData;
-    2: TampilData;
-    3: CariMahasiswa;
+      1: InputData;
+      2: TampilData;
+      3: CariMahasiswa;
     end;
 
-    until pilih = 4;
+  until pilih = 4;
 end.
